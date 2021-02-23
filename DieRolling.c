25ce82aec12287e54595b3f9c8e2e9053a694303
Halim Can Ocaklı <62395643+halimocakli@users.noreply.github.com>
2021-02-23 14:47:12 +0300

    Update DieRolling.c

--- DieRolling.c
+++ DieRolling.c
@@ -3,7 +3,8 @@
 #include<time.h>
 #include<conio.h>
 
-/* Bir cift zarin atisini simule eden bir program yaziniz. Program, ilk ve ikinci zar icin
+/* 
+ * Bir cift zarin atisini simule eden bir program yaziniz. Program, ilk ve ikinci zar icin
  * iki kez rand fonksiyonu kullanmalidir. Daha sonra cikan iki deger toplanmalidir. Her zar
  * 1-6 arasi deger icerdiginden toplam sonuc 2-12 arasinda degisecek, en cok rastlanan sonuc 
  * 7 olacak ve en az 2 ve 12 sonuclari ortaya cikacak. Programimiz iki zari toplamda 36.000
@@ -11,7 +12,7 @@
  * olarak gosteriniz. 
  */
 
-/* 1-6 arasi rastgele sayi uretme fonksiyonu*/
+/* 1-6 arasi rastgele sayi ureten fonksiyon */
 int randomNumberGenerator();
 
 int main()
@@ -21,23 +22,24 @@ int main()
     int i;          /* sayac */
     int j;          /* sayac */
 
-    int sum[13] = {0};  /* iki zar atildiğinda olusacak kombinasyonun sayisini topladigimiz dizi
-                     * su sekilde dusunun, diyelim ki iki zar da 5 geldi ve toplamlari 10 oldu.
-                     * bunu sonucunda iki zarin atilip gelen yuzdeki sayilarin toplaminin on oldugu
-                     * durumu her seferinde bir arttiracagiz. Boylece dizinin 10. indisi +1 olacak 
-                     * ve kombinasyon sayisini tutabilecegiz 
-                     */
+    int sum[13] = {0};                                              /* 
+                                                                     * İki zar atildiğinda olusacak kombinasyonun sayisini topladigimiz dizi
+                                                                     * su sekilde dusunun, diyelim ki iki zar da 5 geldi ve toplamlari 10 oldu.
+                                                                     * bunu sonucunda iki zarin atilip gelen yuzdeki sayilarin toplaminin on oldugu
+                                                                     * durumu her seferinde bir arttiracagiz. Boylece dizinin 10. indisi +1 olacak 
+                                                                     * ve kombinasyon sayisini tutabilecegiz 
+                                                                    */
     
     int expected[13]  = {0, 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1};    /* iki zarin toplaminin gerçekte gelme olasiligi */
 
-    srand(time(NULL));  /* rastgele sayi ureteci ana fonksiyonu */
+    srand(time(NULL));                                              /* Rastgele sayi ureteci ana fonksiyonu */
 
     for(i = 0; i < 36000; ++i)
     {
         firstDie = randomNumberGenerator();
         secondDie = randomNumberGenerator();
 
-        ++sum[firstDie + secondDie];    /* yukarida bahsettigimiz islemi gerceklestiriyoruz */
+        ++sum[firstDie + secondDie];                                /* yukarida bahsettigimiz islemi gerceklestiriyoruz */
     }
 
     printf( "%10s%10s%10s%10s\n", "Sum", "Total", "Expected", "Actual" );

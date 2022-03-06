3b435e253624f8dfa250dd0b9477427878e13c8f
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-03-07 00:12:18 +0300

    Create main.c

---
+++ DataTypeSizesInC/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+
+int main(int argc, char** argv)
+{
+
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("First Konwledge About Size Of Data is 1 Byte = 8 Bit\n\n");
+
+	// sizeof evaluates the size of a variable
+	printf("Size of int: %ld bytes\n", sizeof(intType));
+	printf("Size of float: %ld bytes\n", sizeof(floatType));
+	printf("Size of double: %ld bytes\n", sizeof(doubleType));
+	printf("Size of char: %ld byte\n", sizeof(charType));
+
+	printf("------------------------------\n");
+
+	printf("CHAR_BIT    :   %d\n", CHAR_BIT);
+	printf("CHAR_MAX    :   %d\n", CHAR_MAX);
+	printf("CHAR_MIN    :   %d\n", CHAR_MIN);
+	printf("INT_MAX     :   %d\n", INT_MAX);
+	printf("INT_MIN     :   %d\n", INT_MIN);
+	printf("LONG_MAX    :   %ld\n", (long)LONG_MAX);
+	printf("LONG_MIN    :   %ld\n", (long)LONG_MIN);
+	printf("SCHAR_MAX   :   %d\n", SCHAR_MAX);
+	printf("SCHAR_MIN   :   %d\n", SCHAR_MIN);
+	printf("SHRT_MAX    :   %d\n", SHRT_MAX);
+	printf("SHRT_MIN    :   %d\n", SHRT_MIN);
+	printf("UCHAR_MAX   :   %d\n", UCHAR_MAX);
+	printf("UINT_MAX    :   %u\n", (unsigned int)UINT_MAX);
+	printf("ULONG_MAX   :   %lu\n", (unsigned long)ULONG_MAX);
+	printf("USHRT_MAX   :   %d\n", (unsigned short)USHRT_MAX);
+
+	printf("-----------------------------\n");
+
+	printf("Storage size for float : %d \n", sizeof(float));
+	printf("FLT_MAX     :   %g\n", (float)FLT_MAX);
+	printf("FLT_MIN     :   %g\n", (float)FLT_MIN);
+	printf("-FLT_MAX    :   %g\n", (float)-FLT_MAX);
+	printf("-FLT_MIN    :   %g\n", (float)-FLT_MIN);
+	printf("DBL_MAX     :   %g\n", (double)DBL_MAX);
+	printf("DBL_MIN     :   %g\n", (double)DBL_MIN);
+	printf("-DBL_MAX     :  %g\n", (double)-DBL_MAX);
+	printf("Precision value: %d\n", FLT_DIG);
+
+	return 0;
+}

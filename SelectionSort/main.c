945c2e56075dee5720b1b0f2ed14a8807daa5b9a
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-04-07 13:23:48 +0300

    Update main.c

--- SelectionSort/main.c
+++ SelectionSort/main.c
@@ -1,11 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
 #define MAX 100
 
 // Selection Sort dizi icinde gezerek en kucuk elemani en basa atma yontemi kullanir
 // En kucuk elememani en basa attiktan sonra kontrol etmeye dizinin bir sonraki elemanindan baslar
 
-void selectionSortAlgorithm(int array[], int size)
+void SelectionSortAlgorithm(int[], int);
+void Swap(int*, int*);
+void PrintSortedArray(int[], int);
+
+int main()
+{
+	int array[MAX];
+	int size;
+	int i = 0;
+
+	printf("\nInsert the array size that you are going to create: ");
+	scanf("%d", &size);
+	printf("\n");
+
+	for (i; i < size; ++i)
+	{
+		printf("Insert the %d. element of the array: ", i + 1);
+		scanf("%d", &array[i]);
+	}
+
+	printf("\nElements of the array you have defined are printed below...\n");
+	PrintSortedArray(array, size);
+
+	SelectionSortAlgorithm(array, size);
+
+	printf("\n\nSorted elements of the array printed below...\n");
+	PrintSortedArray(array, size);
+
+	return 0;
+}
+
+void SelectionSortAlgorithm(int array[], int size)
 {
 	int i;
 	int j;
@@ -23,43 +53,22 @@ void selectionSortAlgorithm(int array[], int size)
 				smallestIndex = j;
 			}
 		}
-
-		temp = array[i];
-		array[i] = array[smallestIndex];
-		array[smallestIndex] = temp;
+		Swap(&array[i], &array[smallestIndex]);
 	}
+}
 
-	printf("\n\nGirdiginiz Dizi Degerleri Siralaniyor...\n");
+void Swap(int* aPtr, int* bPtr)
+{
+	int temp = *aPtr;
+	*aPtr = *bPtr;
+	*bPtr = temp;
+}
 
-	for (i = 0; i < size; ++i)
+void PrintSortedArray(int array[], int size)
+{
+	for (int i = 0; i < size; ++i)
 	{
 		printf("%d\t", array[i]);
 	}
 }
 
-int main()
-{
-	int tamSayiDizisi[MAX];
-	int boyut;
-	int i = 0;
-
-	printf("Kac Elemanli Bir Dizi Olusturmak Istiyorsunuz: ");
-	scanf("%d", &boyut);
-	printf("\n");
-
-	for (i; i < boyut; ++i)
-	{
-		printf("Dizinin %d. Elemanini Giriniz: ", i + 1);
-		scanf("%d", &tamSayiDizisi[i]);
-	}
-
-	printf("Girdiginiz Dizi Elemanlari Asagidaki Gibidir...\n");
-
-	for (i = 0; i < boyut; ++i)
-	{
-		printf("%d\t", tamSayiDizisi[i]);
-	}
-
-	selectionSortAlgorithm(tamSayiDizisi, boyut);
-	return 0;
-}
